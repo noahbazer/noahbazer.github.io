b1b55d0a068a3206e696edfb579e87fa536e902a
Noah Bazer <nbazer00@gmail.com>
2023-12-18 21:49:28 -0600

    update header.js and fix stickyheader style for testing

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     let scrollpos = window.scrollY;
-    const header = document.getElementById('stickyHeader');
+    const header = document.querySelector('.header');
     const header_height = header.offsetHeight;
 
     const add_class_on_scroll = () => {
