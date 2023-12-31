12cd0b9534647eb64432333154d494aacee2cd61
Noah Bazer <nbazer00@gmail.com>
2023-12-30 19:57:01 -0600

    fix disclaimer text, fix spacing, add exit button

--- disclaimer.js
+++ disclaimer.js
@@ -1,2 +1,17 @@
-const disclaimer = document.querySelector('.disclaimer'); 
-const disHead = 
+const disclaimer = document.getElementById('disclaimer'); 
+const disHead = document.getElementById('header');
+
+const setDisclaimer = () => {
+    console.log("setDisclaimer called");
+    if (localStorage.disclaimDisabled) {
+        disclaimer.style.display = 'none';
+    }
+    disHead.style.top = `${disclaimer.offsetHeight}px`; 
+}
+
+const clearDisclaimer = () => {
+    console.log("clearDisclaimer called");
+    disclaimer.style.display = 'none';
+}
+
+setDisclaimer();
