2a76e6103defe06a8178766d2c56b7cdf03e7c92
Noah Bazer <nbazer00@gmail.com>
2023-12-30 20:20:07 -0600

    fix flickering disclaimer on refresh

--- disclaimer.js
+++ disclaimer.js
@@ -3,15 +3,27 @@ const disHead = document.getElementById('header');
 
 const setDisclaimer = () => {
     console.log("setDisclaimer called");
-    if (localStorage.disclaimDisabled) {
+    if (window.localStorage.getItem('disclaimDisabled') === '1') {
         disclaimer.style.display = 'none';
+    } else {
+        disclaimer.style.display = 'flex';
     }
     disHead.style.top = `${disclaimer.offsetHeight}px`; 
 }
 
-const clearDisclaimer = () => {
-    console.log("clearDisclaimer called");
+
+
+const removeDisclaimer = () => {
+    console.log("removeDisclaimer called");
     disclaimer.style.display = 'none';
+    disHead.style.top = `0`
+}
+
+const deleteDisclaimer = () => {
+    console.log("deleteDisclaimer called");
+    disclaimer.style.display = 'none';
+    disHead.style.top = `0`
+    window.localStorage.setItem('disclaimDisabled', '1');
 }
 
 setDisclaimer();
