92298ad333d24fdbf4d3e458792dd89f23011f14
Noah Bazer <nbazer00@gmail.com>
2024-02-02 21:10:27 -0600

    begin function of gallery controls

--- src/components/showcase/ShowcaseControls.tsx
+++ src/components/showcase/ShowcaseControls.tsx
@@ -1,4 +1,24 @@
+import { useState } from 'react';
+
 export function ShowcaseControls() {
+  const [activePage, setActivePage] = useState(0);
+
+  const setPage = (page: number) => {
+    setActivePage(page);
+  };
+
+  const nextPage = () => {
+    if (activePage < 2) {
+      setActivePage(activePage + 1);
+    }
+  };
+
+  const lastPage = () => {
+    if (activePage > 0) {
+      setActivePage(activePage - 1);
+    }
+  };
+
   return (
     <div className="carousel-controls">
       <i
@@ -6,16 +26,22 @@ export function ShowcaseControls() {
         onClick={lastPage}
       ></i>
       <i
-        className="fa fa-circle carousel-button"
-        onClick={setPage(0)}
+        className={`fa ${
+          activePage === 0 ? 'fa-circle' : 'fa-regular fa-circle'
+        } carousel-button`}
+        onClick={() => setPage(0)}
       ></i>
       <i
-        className="fa-regular fa-circle carousel-button"
-        onClick={setPage(1)}
+        className={`fa ${
+          activePage === 1 ? 'fa-circle' : 'fa-regular fa-circle'
+        } carousel-button`}
+        onClick={() => setPage(1)}
       ></i>
       <i
-        className="fa-regular fa-circle carousel-button"
-        onClick={setPage(2)}
+        className={`fa ${
+          activePage === 2 ? 'fa-circle' : 'fa-regular fa-circle'
+        } carousel-button`}
+        onClick={() => setPage(2)}
       ></i>
       <i
         className="fa fa-chevron-right"
@@ -25,57 +51,4 @@ export function ShowcaseControls() {
   );
 }
 
-interface activePage {
-  activePage: number;
-}
-
-interface carouselContainer {
-  carouselContainer: HTMLElement;
-}
-
-interface carouselButtons {
-  carouselButtons: HTMLCollectionOf<Element>;
-}
-
-let activePage = 0;
-
-const carouselContainer = document.getElementById('carouselcon');
-const carouselButtons = document.getElementsByClassName('carousel-button');
-
-const setPage = (page: number) => {
-  activePage = page;
-  for (let i = 0; i < carouselButtons.length; i++) {
-    if (i === activePage) {
-      carouselButtons[i].classList.add('fa');
-      carouselButtons[i].classList.remove('fa-regular');
-    } else {
-      carouselButtons[i].classList.remove('fa');
-      carouselButtons[i].classList.add('fa-regular');
-    }
-  }
-  movePage(activePage);
-};
-
-const nextPage = () => {
-  console.log(carouselButtons.length);
-  if (activePage < carouselButtons.length - 1) {
-    activePage++;
-    setPage(activePage);
-  }
-};
-
-const lastPage = () => {
-  if (activePage > 0) {
-    activePage--;
-    setPage(activePage);
-  }
-};
-
-const movePage = (page: number) => {
-  const count = page * 810;
-  const newLeft = `-${count}px`;
-  console.log('New left:', newLeft);
-  if (carouselContainer) {
-    carouselContainer.style.left = newLeft;
-  }
-};
+export default ShowcaseControls;
