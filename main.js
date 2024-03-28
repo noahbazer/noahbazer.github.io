b401bb72800fc22e00914e0066f1bc7c5907373b
Noah Bazer <125102673+noahbazer@users.noreply.github.com>
2024-03-28 16:05:17 -0500

    adjust photo height, add screen logger

--- main.js
+++ main.js
@@ -229,3 +229,10 @@ function downloadResume() {
     downloadButton.innerHTML = 'Download Resume';
   }, 3000);
 }
+
+//function that logs the screen width and height on page load
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log(`Screen width: ${window.innerWidth}`);
+  console.log(`Screen height: ${window.innerHeight}`);
+});
