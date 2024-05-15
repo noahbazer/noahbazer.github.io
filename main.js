fd9e7d8211e03f58c47bea25bb5792669b8ea365
Noah Bazer <125102673+noahbazer@users.noreply.github.com>
2024-05-14 20:12:37 -0500

    Revert "new portfolio"
    This reverts commit 0d89fc9cba5cc180326ab0d3738e93634f300e2d.
    
    e:

--- main.js
+++ main.js
@@ -1,26 +1,223 @@
-const introScreen = document.getElementById('intro-screen');
-const screenLayer = document.getElementById('intro-screen-layer');
-const introName = document.getElementById('introName');
-const introBuffer = document.getElementById('introBuffer');
-const navBar = document.getElementById('navbar');
-const mainContainer = document.getElementById('main-container');
-const headerTitle = document.getElementById('header-title');
-const parallaxOne = document.getElementById('parallax1');
-const parallaxTwo = document.getElementById('parallax2');
+var rellax = new Rellax('.rellax');
 
-const hideTitleName = () => {
-  introName.style.top = '200%';
-  headerTitle.style.top = '0%';
-  introBuffer.style.opacity = 0;
-  setTimeout(() => {
-    introScreen.style.opacity = 0;
-    navBar.style.display = 'flex';
-    parallaxOne.style.top = '0';
-    parallaxTwo.style.top = '0';
-  }, 1000);
-  setTimeout(() => {
-    mainContainer.style.display = 'flex';
-  }, 1500);
+const modals = document.querySelectorAll('.modal');
+const contents = document.querySelectorAll('.modal-content');
+const modalPreviews = document.querySelectorAll('.modal-preview');
+
+const removeModal = function () {
+  for (let i = 0; i < modals.length; i++) {
+    modals[i].style.opacity = 0;
+    modals[i].style.pointerEvents = 'none';
+    contents[i].classList.remove('visible');
+    contents[i].style.pointerEvents = 'none';
+  }
 };
 
-setTimeout(hideTitleName, 3500);
+const showModal = function () {
+  let modal = document.getElementById(`modal`);
+  let modalContent = document.getElementById(`modal-content`);
+  modal.style.opacity = 1;
+  modal.style.pointerEvents = 'all';
+  modalContent.classList.add('visible');
+  modalContent.style.pointerEvents = 'all';
+};
+
+const filterModals = function (filter) {
+  modalPreviews.forEach((card) => {
+    if (filter === 'all') {
+      card.style.display = 'flex';
+    } else if (card.dataset.filter === filter) {
+      card.style.display = 'flex';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+};
+
+const filters = document.querySelectorAll('.sort-link');
+const setActive = (child) => {
+  for (let i = 0; i < filters.length; i++) {
+    if (i === child - 1) {
+      filters[i].id = 'sort-active';
+    } else {
+      filters[i].id = '';
+    }
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Disable default scroll restoration
+  if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+
+  // Scroll to the top on page load
+  window.scrollTo(0, 0);
+
+  let currentSection = 0;
+  let isScrolling = false;
+  const sections = document.querySelectorAll('.section');
+  const headerContainer = document.getElementById('header-container');
+  const scrollHint = document.querySelector('.scroll-hint p');
+
+  function smoothScroll(targetSection) {
+    if (isScrolling) return;
+    isScrolling = true;
+
+    // Use getBoundingClientRect to calculate the target position
+    const targetPosition =
+      sections[targetSection].getBoundingClientRect().top + window.scrollY;
+
+    const currentPosition = window.scrollY;
+    const distance = targetPosition - currentPosition;
+    const duration = 1000;
+
+    let startTime;
+
+    function animation(currentTime) {
+      if (startTime === undefined) startTime = currentTime;
+      const elapsedTime = currentTime - startTime;
+      const ease = easeInOutCubic(
+        elapsedTime,
+        currentPosition,
+        distance,
+        duration
+      );
+      window.scrollTo(0, ease);
+
+      if (elapsedTime < duration) {
+        requestAnimationFrame(animation);
+      } else {
+        isScrolling = false;
+        updateSectionVisibility(); // Update visibility after the animation is complete
+      }
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t * t + b;
+      t -= 2;
+      return (c / 2) * (t * t * t + 2) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+
+  function handleScroll(event) {
+    event.preventDefault();
+    const delta = event.deltaY;
+
+    if (isScrolling) return; // Ignore additional scroll events during animation
+
+    if (delta > 0 && currentSection < sections.length - 1) {
+      currentSection++;
+      smoothScroll(currentSection);
+    } else if (delta < 0 && currentSection > 0) {
+      currentSection--;
+      smoothScroll(currentSection);
+    }
+
+    // Update header class based on scroll position
+    updateHeaderClass();
+
+    // Update scroll-hint visibility based on current section
+    updateScrollHintVisibility();
+  }
+
+  window.addEventListener('resize', function () {
+    const targetPosition =
+      sections[currentSection].getBoundingClientRect().top + window.scrollY;
+    window.scrollTo(0, targetPosition);
+  });
+  function updateHeaderClass() {
+    const scrolledClass = 'header-container-scrolled';
+    const scrollThreshold = 5;
+
+    if (window.scrollY >= scrollThreshold) {
+      headerContainer.classList.add(scrolledClass);
+    } else {
+      headerContainer.classList.remove(scrolledClass);
+    }
+  }
+
+  function updateScrollHintVisibility() {
+    const targetSection = 1; // Adjust the target section index as needed
+
+    if (currentSection === targetSection) {
+      // Set the opacity or hide the scroll hint element
+      scrollHint.style.opacity = '0'; // Adjust as needed for your preferred opacity value
+    } else {
+      // Reset the opacity or show the scroll hint element
+      scrollHint.style.opacity = '1'; // Adjust as needed for your preferred opacity value
+    }
+  }
+
+  function updateSectionVisibility() {
+    // Toggle the visible class for specific elements in the second section
+    const aboutMe2Images = document.querySelector('.about-me-2-images');
+    const aboutMe2Content = document.querySelector('.about-me-2-content');
+
+    const aboutMe3Images = document.querySelector('.about-me-3-images');
+    const aboutMe3Content = document.querySelector('.about-me-3-content');
+
+    const aboutMe4Images = document.querySelector('.about-me-4-images');
+    const aboutMe4Content = document.querySelector('.about-me-4-content');
+
+    const aboutMe5Images = document.querySelector('.about-me-5-images');
+    const aboutMe5Content = document.querySelector('.about-me-5-content');
+
+    if (currentSection === 1) {
+      aboutMe2Images.style.opacity = '1';
+      aboutMe2Content.style.opacity = '1';
+    }
+
+    if (currentSection === 2) {
+      aboutMe3Images.style.opacity = '1';
+      aboutMe3Content.style.opacity = '1';
+    }
+
+    if (currentSection === 3) {
+      aboutMe4Images.style.opacity = '1';
+      aboutMe4Content.style.opacity = '1';
+    }
+
+    if (currentSection === 4) {
+      aboutMe5Images.style.opacity = '1';
+      aboutMe5Content.style.opacity = '1';
+    }
+  }
+
+  window.addEventListener('wheel', handleScroll, { passive: false });
+  window.addEventListener('scroll', updateHeaderClass);
+  window.addEventListener('scroll', updateScrollHintVisibility);
+  window.addEventListener('scroll', updateSectionVisibility);
+});
+
+const disclaimer = document.getElementById('disclaimer');
+
+const hideDisclaimer = () => {
+  disclaimer.style.bottom = '-100px';
+};
+
+const deleteDisclaimer = () => {
+  localStorage.setItem('disclaimer', 'true');
+  hideDisclaimer();
+};
+
+const checkDisclaimer = () => {
+  if (localStorage.getItem('disclaimer') !== 'true') {
+    disclaimer.style.bottom = '0';
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  checkDisclaimer();
+});
+
+function downloadResume() {
+  let downloadButton = document.getElementById('resume');
+  downloadButton.innerHTML = 'Downloading...';
+  setTimeout(() => {
+    downloadButton.innerHTML = 'Download Resume';
+  }, 3000);
+}
