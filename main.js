05688c7d80432d78375599e1bdcdf5b2cea6ed20
Noah Bazer <125102673+noahbazer@users.noreply.github.com>
2024-07-19 23:39:43 -0400

    gold splashes, intro screen, etc

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+function setTextAnimation(
+  delay,
+  duration,
+  strokeWidth,
+  timingFunction,
+  strokeColor,
+  repeat
+) {
+  let paths = document.querySelectorAll('path');
+  let mode = repeat ? 'infinite' : 'forwards';
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i];
+    const length = path.getTotalLength();
+    path.style['stroke-dashoffset'] = `${length}px`;
+    path.style['stroke-dasharray'] = `${length}px`;
+    path.style['stroke-width'] = `${strokeWidth}px`;
+    path.style['stroke'] = `${strokeColor}`;
+    path.style[
+      'animation'
+    ] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+    path.style['animation-delay'] = `${i * delay}s`;
+  }
+}
+setTextAnimation(0.1, 2.2, 2, 'linear', '#ffe7a3', false);
+
 var rellax = new Rellax('.rellax');
 
 const modals = document.querySelectorAll('.modal');
